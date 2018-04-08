4ec8e0373f2ba7b8d3c6ffb9f9997ef96974e83a
PuyaYavari <pouyayavari73@gmail.com>
2018-04-08 05:26:04 +0300

    First commit

--- AnketSistemi/LOGIN/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
-def home(request):
-    if request.method == 'post':
-        HttpResponse('hello')
-    return render(request, 'LOGIN/LOGIN.html')
